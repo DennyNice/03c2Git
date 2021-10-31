84017e205c1356347320cf14b0b17a04d0fa7400
DennyNice <dennynice@icloud.com>
2021-10-31 16:50:42 +0100

    Initial commit

---
+++ src/com/company/KlasaGlowna.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class KlasaGlowna {
+    private String pole ="Nazwa";
+
+    @Override
+    public String toString() {
+        return "Testowa{" + "pole" +pole + "/"+ '}';
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        KlasaGlowna kg = new KlasaGlowna();
+
+        System.out.println(kg);
+    }
+}
